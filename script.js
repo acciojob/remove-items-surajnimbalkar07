82683fea5594f3f2a25aff0baece9820d87448c1
Suraj Nimbalkar <136218136+surajnimbalkar07@users.noreply.github.com>
2025-11-10 21:02:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const select = document.getElementById("colorSelect");
+    const button = document.getElementById("removeBtn");
+
+    button.onclick = function() {
+      select.remove(select.selectedIndex);
+    }
